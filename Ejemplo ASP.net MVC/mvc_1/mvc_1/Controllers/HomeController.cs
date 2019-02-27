3802921c16c5b422f3639af77bd2562f8e39878f
bsandovalmora <bsandovalmora@gmail.com>
2019-02-27 08:59:37 -0600

    Actualizado

---
+++ Ejemplo ASP.net MVC/mvc_1/mvc_1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_1.Models;
+
+namespace mvc_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+        public ActionResult CargaDatos()
+        {
+	            string nombre = Request.Form["nombre"].ToString();
+                string comentarios = Request.Form["comentarios"].ToString();
+	            LibroVisitas libro = new LibroVisitas();
+	            libro.Grabar(nombre, comentarios);  
+	            return View();
+	        }
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["libro"] = todo;
+            return View();
+        }
+    }
+}
+
---
+++ Ejemplo ASP.net MVC/mvc_1/mvc_1/Models/LibroVisitas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.IO;
+
+
+namespace mvc_1.Models
+{
+    public class LibroVisitas
+    {
+        public void Grabar(string nombre, string comentarios)
+        {
+            StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt", true);
+            archivo.WriteLine("Nombre:" + nombre + "<br>Comentarios:" + comentarios + "<hr>");
+            archivo.Close();
+        }
+        public string Leer()
+        {
+            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
+            string todo = archivo.ReadToEnd();
+            archivo.Close();
+            return todo;
+        }
+    }
+}
+
